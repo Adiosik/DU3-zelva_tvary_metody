9492ba7741577b89167b5635053e3fe9cf0825a6
PetraSim1 <117669238+PetraSim1@users.noreply.github.com>
2023-02-23 20:18:13 +0100

    Initial commit

---
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -0,0 +1,16 @@
+package cz.czechitas.turtle;
+
+import cz.czechitas.turtle.engine.*;
+
+public class HlavniProgram {
+    private Turtle zofka = new Turtle();
+
+    public void start() {
+
+    }
+
+    public static void main(String[] args) {
+        new HlavniProgram().start();
+    }
+
+}
